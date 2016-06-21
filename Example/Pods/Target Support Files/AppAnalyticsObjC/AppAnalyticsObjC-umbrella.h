f18a360819c8440614b8ad25d5fa34ecd6ad5d5c
Vahid Sayad <vahidgiga@gmail.com>
2016-06-21 11:42:31 +0430

    fixing travic

--- Example/Pods/Target Support Files/AppAnalyticsObjC/AppAnalyticsObjC-umbrella.h
+++ Example/Pods/Target Support Files/AppAnalyticsObjC/AppAnalyticsObjC-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "AppAnalyticsObjC.h"
 
 FOUNDATION_EXPORT double AppAnalyticsObjCVersionNumber;
 FOUNDATION_EXPORT const unsigned char AppAnalyticsObjCVersionString[];
